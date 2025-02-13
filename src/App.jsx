643d075d5db4cbb1cc529ecbb922c78ea0358fde
abhilasha1235 <abhilashap868@gmail.com>
2025-02-13 18:15:09 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import Phone from "./components/Phone";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -29,6 +30,7 @@ const App = () => {
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
+      <Phone />
       <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery} />
       <Testimonials data={landingPageData.Testimonials} />
---
+++ src/components/Phone.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Phone() {
+  return (
+    <div className="container-app">
+    
+      <div className="text-section">
+        <h1>
+          Download the <br /> Latest Version <br /> of the App from
+        </h1>
+      </div>
+      <button className="discover-btn">Discover More</button>
+
+
+      <div className="image-section">
+        <img src="img/Phone.png" alt="Phone App Preview" className="phone-image" />
+      </div>
+    </div>
+  )
+}
+
+export default Phone;
